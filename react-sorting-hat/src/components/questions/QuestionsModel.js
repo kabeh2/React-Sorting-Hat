bd962508c4f618ac20032da881f1f23e58b40bac
kabeh2 <mattcnk@hotmail.com>
2019-09-04 23:45:32 -0400

    Finished Welcme Component Markup, and routing of questions

---
+++ react-sorting-hat/src/components/questions/QuestionsModel.js
@@ -0,0 +1,164 @@
+export const QuestionsModel = [
+  {
+    id: 1,
+    question: "Question 1?",
+    set: "one",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  },
+  {
+    id: 2,
+    question: "Question 2?",
+    set: "two",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  },
+  {
+    id: 3,
+    question: "Question 3?",
+    set: "three",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  },
+  {
+    id: 4,
+    question: "Question 4?",
+    set: "four",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  },
+  {
+    id: 5,
+    question: "Question 5?",
+    set: "five",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  },
+  {
+    id: 6,
+    question: "Question 6?",
+    set: "six",
+    answers: [
+      {
+        value: "a",
+        answer: "a) answer 1",
+        count: 1
+      },
+      {
+        value: "b",
+        answer: "b) answer 2",
+        count: 2
+      },
+      {
+        value: "c",
+        answer: "c) answer 3",
+        count: 3
+      },
+      {
+        value: "d",
+        answer: "d) answer 4",
+        count: 4
+      }
+    ]
+  }
+];
