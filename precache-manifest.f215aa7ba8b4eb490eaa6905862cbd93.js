703bba906e7c4e78810e9d0b0eb05ef3bf3e4a54
kabeh2 <mattcnk@hotmail.com>
2019-09-14 02:18:07 -0400

    Updates

---
+++ precache-manifest.f215aa7ba8b4eb490eaa6905862cbd93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c156070d91797139cd098698d22ef9",
+    "url": "/React-Sorting-Hat/index.html"
+  },
+  {
+    "revision": "fa507c4e3afd258ed50f",
+    "url": "/React-Sorting-Hat/static/css/main.63a40524.chunk.css"
+  },
+  {
+    "revision": "7804381cdf478bf43be6",
+    "url": "/React-Sorting-Hat/static/js/2.0542a6e8.chunk.js"
+  },
+  {
+    "revision": "fa507c4e3afd258ed50f",
+    "url": "/React-Sorting-Hat/static/js/main.d5ebc28c.chunk.js"
+  },
+  {
+    "revision": "1b1f24e306c56b41b7ec",
+    "url": "/React-Sorting-Hat/static/js/runtime~main.a2d040da.js"
+  },
+  {
+    "revision": "3efe6c36dffee0c85b0208878f12ea52",
+    "url": "/React-Sorting-Hat/static/media/gryffindor-crest.3efe6c36.png"
+  },
+  {
+    "revision": "db5ee7e31712d471380f6cf85cccf5b1",
+    "url": "/React-Sorting-Hat/static/media/hufflepuff-crest.db5ee7e3.png"
+  },
+  {
+    "revision": "8f9cd23983f62aeb429648f63935667f",
+    "url": "/React-Sorting-Hat/static/media/ravenclaw-crest.8f9cd239.png"
+  },
+  {
+    "revision": "43c52107ed68da0c472b2805b0b60b8a",
+    "url": "/React-Sorting-Hat/static/media/slytherin-crest.43c52107.png"
+  },
+  {
+    "revision": "7243f6fa9514716a8df87a9dcdcc93fc",
+    "url": "/React-Sorting-Hat/static/media/the_sorting_hat_by_sahinduezguen.7243f6fa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-sorting-hat/precache-manifest.d2c2736963b1e5a54ffbb363bb504943.js"
+  "/React-Sorting-Hat/precache-manifest.f215aa7ba8b4eb490eaa6905862cbd93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sorting-hat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Sorting-Hat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
